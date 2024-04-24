1298440347b2d98f408fc2d356cb7fe5e2d82984
Wangyuran <13315562276@163.com>
2024-04-24 16:09:18 +0800

    Assistant initial

---
+++ CCGPT/Assistant.py
@@ -0,0 +1,15 @@
+class Assistant():
+    def __init__(self) -> None:
+        pass
+
+    def train(self):
+        pass
+
+    def chat(self, message, flag):
+        print("from GTP:", message)
+        responce = input("请输入硬件专家的指导：")
+        return responce
+        pass
+
+    def start(self):
+        pass
