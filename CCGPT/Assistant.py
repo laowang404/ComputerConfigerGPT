c9decaf2f05c1114012b57cbe126618aef8a10eb
Wangyuran <13315562276@163.com>
2024-04-29 11:24:15 +0800

    prompt fix

--- CCGPT/Assistant.py
+++ CCGPT/Assistant.py
@@ -7,10 +7,10 @@ import string
 class Assistant():
     def __init__(self) -> None:
         self.client = ZhipuAI(api_key="9a0cb62c576a25d83a00ac432c5fcc7a.Wkl0gAJVDuvu1EAS")
-        self.sys_prompt = "你是一名专业的$hardware硬件专家，知晓CPU的各种细节，能够根据需求提供合适的$hardware选择。" \
+        self.sys_prompt = "你是一名专业的$hardware硬件专家，知晓$hardware的各种细节，能够根据需求提供合适的$hardware选择。" \
                           "你会定期收到以\"信息更新：\"开头的信息，该信息会告诉你当前市场上主流的$hardware信息，包括性能、价格等，此信息为Json格式。" \
                           "你需要牢记这些信息。" \
-                          "有一个装机专家会按照需求向你询问$hardware的信息，你需要根据学到的内容，向他提供一个或多个合适的CPU选择。" \
+                          "有一个装机专家会按照需求向你询问$hardware的信息，你需要根据学到的内容，向他提供一个或多个合适的$hardware选择。" \
                           "对于每个选择，你只需要将$hardware的详细信息和价格告诉他即可，不同的$hardware选择之间用易于理解的方式分隔。" \
                           "回复方式参照你的同行CPU硬件专家：\"英特尔酷睿i7 14700 KF 最高睿频5.6GHz，20核28线程，TDP 125W，插槽类型LGA 1700，当前价格2698；AMD Ryzen 9 7900X 最高睿频5.6GHz，12核24线程，TDP 170W，插槽类型Sokect AM5，当前价格2599。\"" \
                           "请注意使用自然语言进行交流 而不是复述Json信息，选项不超过5个。"\
